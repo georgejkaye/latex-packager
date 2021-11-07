74333a7e2bb903186c678695b622b20021ef4eba
George Kaye <georgejkaye@gmail.com>
2021-11-07 11:39:27 +0000

    Added argument to choose refs output

---
+++ src/figs.py
@@ -0,0 +1,9 @@
+import sys
+import re
+
+file = sys.argv[1]
+
+tikzfig = 'tikzfig\{(.*?)\}'
+
+with open(file) as f:
+    text
--- src/refs.py
+++ src/refs.py
@@ -2,9 +2,23 @@ import sys
 import re
 
 bib = '(@[a-z]*{([a-z0-9]*),\n(.*\n)*?})'
+fname = '(.*?).'
 
 file = sys.argv[1]
 refs = sys.argv[2]
+
+if len(sys.argv) == 4:
+    outp = sys.argv[3]
+else:
+    outp = "refs.bib"
+
+
+if len(filematches) == 0:
+    filename = file
+else:
+    filename = filematches[-1][1]
+
+filename = filename + ".bib"
 
 doclines = []
 
@@ -24,7 +38,9 @@ with open(refs) as rf:
                 bibs.append(match[0])
                 break
 
-with open("refs.bib", "w") as f:
+print("Writing minimal bib file to " + outp)
+
+with open(outp, "w") as f:
     for b in bibs[:-1]:
         f.write(b + "\n\n")
     f.write(bibs[-1])
