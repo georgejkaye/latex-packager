170f795a835d4af624e6641f468afe0abb8076c7
George Kaye <georgejkaye@gmail.com>
2021-12-03 09:45:19 +0000

    Updated init script

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ This repo contains some scripts I wrote to make my life easier.
 
 (They might have been done before, I don't care)
 
+## `init.py`
+
+Initialises an empty latex file and readme, using a given title and author.
+This is designed to work with my personal `latex-template` github template.
+
+The readme details how to handle the submodules, and contains a link to `repo.pdf` and `repo.zip` in the latest release.
+
 ## `package.sh`
 
 Bundles a latex file and all its dependencies into a minimal zip package.
@@ -26,7 +33,6 @@ locs1970
 ```
 
 ## `refs.py`
-
 
 Parses a latex file and a bib file and makes a new bib file with only the references used in the latex file.
 
--- src/init.py
+++ src/init.py
@@ -14,3 +14,24 @@ with open("main.tex", "w+") as f:
     f.write("\\begin{document}\n")
     f.write(tab + "\\maketitle\n")
     f.write("\\end{document}\n")
+
+
+with open("README.md", "w+") as f:
+    f.write("# " + title + "\n")
+    f.write("\n")
+    f.write("This repo has been created from the 'latex-template' repo.\n")
+    f.write(
+        "This project uses *submodules* to share diagrams and things between repos.\n")
+    f.write("To make sure you have them all pulled, do this to initialise them:\n\n")
+    f.write("```sh\n")
+    f.write("git submodule update --init\n")
+    f.write("```\n\n")
+    f.write("Occasionally you might want to do this to update the submodules:\n\n")
+    f.write("```sh\n")
+    f.write("git submodule foreach git pull origin main\n")
+    f.write("```\n\n")
+    f.write("## Latest release\n\n")
+    f.write("* [PDF file](https://github.com/georgejkaye/" +
+            title + "/releases/latest/download/" + title + ".pdf)\n")
+    f.write("* [Latest source package](https://github.com/georgejkaye/" +
+            title + "/releases/latest/download/" + title + ".zip)\n")
